789144b13bbf2a918e4835c495b677dfc8fbc79f
Ivan Estropigan <iestropigan@rrc.ca>
2025-05-16 19:09:28 -0500

    First initalized Module-2.py, Documentation, Cross Asterisks and kg to pounds.

---
+++ Module-2.py
@@ -0,0 +1,12 @@
+"""This is a module practice questions"""
+
+__author__ = "Ivan Estropigan"
+__version__ = "1.0"
+__Credits__ = "COMP-1327, w3school"
+
+print("*", "","","*","\n"
+      "", "*", "*", "","\n"
+      "", "", "*","\n"
+      "", "*", "*", "","\n"
+      "*", "","","*","\n")
+
