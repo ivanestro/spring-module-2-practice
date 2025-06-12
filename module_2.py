e90bafa7299963c4c11d97b0914144f1351d06b1
ivanestro <iestropigan@rrc.ca>
2025-06-11 20:13:54 -0500

    Update Starting Question 16, I have done the previous questions have started from question 8.

--- module_2.py
+++ module_2.py
@@ -131,15 +131,190 @@ else:
     print("invalid")
 
 
-# Question 8 
+# Question 8
 print("\nWelcome to Comparing two words that are shorter.")
 
 word = input("Please write a word: ")
 word2 = input("Write another word: ")
 
-if word > word2: 
-    print(f"Your first word is shorter than word 2.{len(word)} The words are {word}, {word2}")
+if word > word2:
+    print(f"Your first word is bigger than your second word. {len(word)} The words are {word}, {word2}")
 
 elif word2 > word:
-    print(f"The length of your second word is: {len(word2)} The words are {word}, {word2}")
+    print(f"The length of your second word is: {len(word2)} The words are {word} and {word2}")
 
+else:
+    print("Hello")
+
+# Question 9
+"""Question 9: 
+Write a program that creates a domain name with anything inputted.
+
+args:
+input_name = input("Please Enter a domain name. ")
+domain_name = "www." + input_name + ".com"
+
+return: Your domain name is {domain_name}
+"""
+print("\nWelcome to multiple domain names.")
+
+input_name = input("Please Enter a domain name. ")
+
+domain_name = "www." + input_name + ".com"
+
+print(f"Your domain name is {domain_name}")
+
+# Question 10
+"""Question 10: 
+Write a program that reads from the keyboard's input
+should only read in uppercase, lowercase and original word.
+
+args:
+keyword = " "
+
+return: 
+print(keyword.upper())
+print(keyword.lower())
+print(keyword)
+"""
+
+keyword = " "
+
+print("\nWelcome to reads from keyboard input and returns them.")
+keyword = input("")
+
+print(keyword.upper())
+print(keyword.lower())
+print(keyword)
+
+# Question 11
+"""Question 11: 
+Write a program that accepts number as an input the computes and outputs a cube of that number.
+
+args:
+
+
+return: 
+
+"""
+
+print("Choose any number and I will cube them.")
+
+number = int(input("Input any number here "))
+
+number = number * number * number
+
+print(number)
+
+# Question 12
+"""Question 11: 
+Write a program that reads file name from the keyboard
+If user inputs index.html or MyClass.Java it should only output html or java
+
+args:
+user = user.split(".")
+
+return:
+user input 
+"""
+
+print("\nWelcome to Question 12")
+user = input("Write any of the following [index.html] | [MyClass.java] ")
+
+user = user.split(".")
+
+print(user[1])
+
+# Question 13
+"""Question 13: 
+Write three integers such as nickel, dime, quarter
+convert total coin amount to dollars and output the result
+
+args:
+money = {"nickel": [0.05], "dime": [0.10] , "quarter": [0.25]}
+money["nickel"].append(userNickel)
+money["dime"].append(userDime)
+money["quarter"].append(userQuarter)
+
+return:
+You have total: ${total:.2f}
+"""
+
+print("\nQuestion 13 Converts total coin amount to dollars")
+money = {"nickel": [0.05], "dime": [0.10] , "quarter": [0.25]}
+
+userNickel = int(input("How many nickel do you want? "))
+userDime = int(input("How many dime do you want? "))
+userQuarter = int(input("How many quarters do you want? "))
+
+money["nickel"].append(userNickel)
+money["dime"].append(userDime)
+money["quarter"].append(userQuarter)
+
+total = (
+    money["nickel"][0] * money["nickel"][1] +
+    money["dime"][0] * money["dime"][1] +
+    money["quarter"][0] * money["quarter"][1]
+)
+
+print(money)
+
+print(f"You have total: ${total:.2f}")
+
+# Question 14
+"""Question 14:
+Write a program that prints the average for user input.
+
+args:
+number1 = float(input("Give me a number "))
+number2 = float(input("Give me a second number "))
+number3 = float(input("Give me a third number "))
+average = (number1 + number2 + number3) / 3
+
+return:
+Your average is: {average:.2f}
+"""
+
+print("\nQuestion 14: Calculates the average")
+number1 = float(input("Give me a number "))
+number2 = float(input("Give me a second number "))
+number3 = float(input("Give me a third number "))
+
+average = (number1 + number2 + number3) / 3
+
+print(f"Your average is: {average:.2f}")
+
+# Question 15
+"""Question 14:
+Write a program to shoot a basketball and how much the basketball player made a shot to percentage
+
+args:
+numberOfShots = int(input("Number of Shots Taken: "))
+shotsMade = int(input("Shots Made: "))
+totalPercent = shotsMade / numberOfShots * 100
+
+return:
+Your shooting percentage is: %{totalPercent}
+"""
+
+print("\nQuestion 15 Basketball")
+
+numberOfShots = int(input("Number of Shots Taken: "))
+shotsMade = int(input("Shots Made: "))
+
+totalPercent = shotsMade / numberOfShots * 100
+
+print(f"Your shooting percentage is: %{totalPercent}")
+
+# Question 16
+"""Question 16:
+Write a program to shoot a basketball and how much the basketball player made a shot to percentage
+
+args:
+
+
+return:
+"""
+
+fv = ''
+pv = ''
